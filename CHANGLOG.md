f47870b312bd643a8b2588b76eab6a94aa891ca7
Michael Berkowitz <michael.berkowitz@gmail.com>
2018-06-10 00:25:40 -0400

    Version 1.0.0

---
+++ CHANGLOG.md
@@ -0,0 +1,8 @@
+# 1.0.0
+
+Adds API functions:
+
+* `PostApocalypticNovelGenerator.generate/1`
+* `PostApocalypticNovelGenerator.generate_random/0`
+
+source: https://electricliterature.com/discover-the-plot-of-your-post-apocalyptic-novel-with-our-handy-chart-dfbcd2629aa0
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PostApocalypticNovelGenerator.MixProject do
   def project do
     [
       app: :post_apocalyptic_novel_generator,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
